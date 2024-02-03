ff9c1023c174a92fdc2496ed60d5b5258ada2119
yuorei <108039575+yuorei@users.noreply.github.com>
2024-02-03 17:14:00 +0900

    クッキーをlocalstorageに変更

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,10 +4,8 @@ import { MainNav } from "./main-nav"
 import UserRegisterButton from "./user-register"
 import UploadVideo from "./upload-video"
 import Login from "./login"
-import { useCookies } from "react-cookie"
 
 export default function Header() {
-  const [cookies] = useCookies(["idToken"]);
   return (
     <header className="sticky flex justify-center border-b">
       <div className="flex items-center justify-between w-full h-16 px-4 mx-auto sm:px-6">
--- src/app/components/upload-video.tsx
+++ src/app/components/upload-video.tsx
@@ -1,16 +1,29 @@
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
-import { Button } from "./ui/button"
-import CustomLink from "./custom-link"
-import Image from "next/image"
-import { useCookies } from "react-cookie"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import CustomLink from "./custom-link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function UploadVideoButton() {
-    const [cookies] = useCookies(["idToken"]);
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // localStorageからトークンを取得し、状態にセットします
+            const storedToken = localStorage.getItem('token');
+            if (storedToken) {
+                setToken(storedToken);
+            } else {
+                console.log("token not found");
+            }
+        }
+    }, []);
+
     return (
-        <CustomLink href={!cookies.idToken ? "/login" : "/video/upload"}>
+        <CustomLink href={!token ? "/login" : "/video/upload"}>
             <Button variant="ghost" className="p-0">
                 <Image src="/logo.png" alt="Home" width="32" height="32" />
             </Button>
         </CustomLink>
-    )
+    );
 }
