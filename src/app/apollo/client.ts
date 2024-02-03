785d00c3583745140622cba2d0f4636251178944
yuorei <108039575+yuorei@users.noreply.github.com>
2024-02-03 17:08:45 +0900

    ヘッダー追加

--- src/app/apollo/client.ts
+++ src/app/apollo/client.ts
@@ -1,9 +1,24 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
+import { setContext } from '@apollo/client/link/context';
+
+const url = `${process.env.NEXT_PUBLIC_API_URL}/graphql`
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const uploadLink = createUploadLink({
+  uri: url,
+});
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: createUploadLink({
-    uri: "https://video-api.yuorei.com/graphql",
-  }),
+  link: authLink.concat(uploadLink),
 });
