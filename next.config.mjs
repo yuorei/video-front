faacfc3776132d9c758d0adc3fc274b7b5170351
yuorei <108039575+yuorei@users.noreply.github.com>
2024-02-03 17:14:18 +0900

    video-storage.yuorei.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // video-storage.yuorei.com追加
+    images: {
+        domains: ['video-storage.yuorei.com'],
+    },
+};
 
 export default nextConfig;
