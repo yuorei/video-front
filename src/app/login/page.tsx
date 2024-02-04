a617fd497bf911af197ea36b08c187f2d9876728
yuorei <108039575+yuorei@users.noreply.github.com>
2024-02-04 11:12:04 +0900

    fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -98,7 +98,7 @@ const LoginForm: React.FC = () => {
                             onClick={handleLogin}
                             className="w-full px-4 py-2 tracking-wide text-gray-900 transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400"
                         >
-                            Login
+                            ログイン
                         </button>
                     </div>
                     <div className="mt-6">
@@ -106,7 +106,7 @@ const LoginForm: React.FC = () => {
                             <button
                                 className="w-full px-4 py-2 tracking-wide text-gray-900 transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400"
                             >
-                                ユーザー登録
+                                アカウント登録
                             </button>
                         </CustomLink>
                     </div>
