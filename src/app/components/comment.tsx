beee2076136cc764d3c3271c4de92bfa0e5ef04d
yuorei <108039575+yuorei@users.noreply.github.com>
2024-02-12 11:13:54 +0900

    コメント隠す機能

--- src/app/components/comment.tsx
+++ src/app/components/comment.tsx
@@ -65,6 +65,7 @@ const Comments: React.FC<CommentsProps> = ({ videoID }) => {
     });
 
     const [text, setText] = useState('');
+    const [showComments, setShowComments] = useState<boolean>(window.innerWidth > 1280);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -80,43 +81,54 @@ const Comments: React.FC<CommentsProps> = ({ videoID }) => {
         }
     };
 
+    const toggleComments = () => {
+        setShowComments(prevState => !prevState);
+    };
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
 
     return (
         <div className="mt-4">
             <h2 className="text-lg font-semibold mb-2">{data?.commentsByVideo.length}件のコメント</h2>
-            <form onSubmit={handleSubmit} className="mt-4">
-                <textarea
-                    value={text}
-                    onChange={e => setText(e.target.value)}
-                    className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 bg-black text-white placeholder-gray-400"
-                    rows={4}
-                    placeholder="コメントする..."
-                />
-                <button
-                    type="submit"
-                    className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-                >
-                    コメント
-                </button>
-            </form>
-            {data && data.commentsByVideo.map(comment => (
-                <div key={comment.id} className="flex items-start mb-4">
-                    <div className="w-10 h-10 mr-3 rounded-full overflow-hidden">
-                        <CustomLink href={`/channel/${comment.user.id}`}>
-                            <img src={comment.user.profileImageURL} alt={comment.user.name} className="w-full h-full object-cover" />
-                        </CustomLink>
-                    </div>
-                    <div>
-                        <div className="flex items-center">
-                            <p className="text-white mr-2">{comment.user.name}</p>
-                            <p className="text-gray-400 text-sm">{timeAgo(comment.createdAt)}</p>
+            <button onClick={toggleComments} className="bg-black border border-white text-white font-bold py-2 px-4 rounded">
+                {showComments ? 'コメントを隠す' : 'コメントを表示する'}
+            </button>
+            {showComments && (
+                <>
+                    <form onSubmit={handleSubmit} className="mt-4">
+                        <textarea
+                            value={text}
+                            onChange={e => setText(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 bg-black text-white placeholder-gray-400"
+                            rows={4}
+                            placeholder="コメントする..."
+                        />
+                        <button
+                            type="submit"
+                            className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                        >
+                            コメント
+                        </button>
+                    </form>
+                    {data && data.commentsByVideo.map(comment => (
+                        <div key={comment.id} className="flex items-start mb-4">
+                            <div className="w-10 h-10 mr-3 rounded-full overflow-hidden">
+                                <CustomLink href={`/channel/${comment.user.id}`}>
+                                    <img src={comment.user.profileImageURL} alt={comment.user.name} className="w-full h-full object-cover" />
+                                </CustomLink>
+                            </div>
+                            <div>
+                                <div className="flex items-center">
+                                    <p className="text-white mr-2">{comment.user.name}</p>
+                                    <p className="text-gray-400 text-sm">{timeAgo(comment.createdAt)}</p>
+                                </div>
+                                <p className="text-white">{comment.text}</p>
+                            </div>
                         </div>
-                        <p className="text-white">{comment.text}</p>
-                    </div>
-                </div>
-            ))}
+                    ))}
+                </>
+            )}
         </div>
     );
 };
