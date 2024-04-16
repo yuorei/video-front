8f308b97c548d37d50f4de4dcf52f778e19d4de7
yuorei <108039575+yuorei@users.noreply.github.com>
2024-04-16 10:40:23 +0900

    Add 'use client' import statement to HLSPlayer, comment, and error components

--- src/app/components/comment.tsx
+++ src/app/components/comment.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { gql } from '@apollo/client';
--- src/app/components/error.tsx
+++ src/app/components/error.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import CustomLink from './custom-link';
 import { notFound } from 'next/navigation';
