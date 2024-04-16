262c45cdba3b30757289aa73dd8e2140d78c33f4
yuorei <108039575+yuorei@users.noreply.github.com>
2024-04-16 21:55:50 +0900

    log

--- src/app/video/[id]/opengraph-image.tsx
+++ src/app/video/[id]/opengraph-image.tsx
@@ -5,35 +5,42 @@ import React from 'react'
 export const contentType = 'image/png'
 
 export const size = {
-    width: 1200,
-    height: 630,
-  };
+  width: 1200,
+  height: 630,
+};
 
 export default async function og({ params }: { params: { id: string } }) {
-const id = params.id
-let video = await getVideo({id})
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontSize: 88,
-          background: '#fff',
-          color: '#000',
-        }}
-      >
-        <img src={video?.thumbnailImageURL ||'/opengraph-image.jpg'} alt={video?.title} />
-      </div>
-    ),
-    {
+  const id = params.id
+  let video = await getVideo({ id })
+  console.log(video)
+  try {
+    let imageResponse = new ImageResponse(
+      (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: 88,
+            background: '#fff',
+            color: '#000',
+          }}
+        >
+          <img src={video?.thumbnailImageURL || '/opengraph-image.jpg'} alt={video?.title} />
+        </div>
+      ),
+      {
         ...size,
-    }
-  )
+      }
+    )
+
+    return imageResponse
+
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const runtime = 'nodejs'
