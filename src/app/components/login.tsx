00605bfe443c1c676663dfcae1619dbdbc5cc2c2
yuorei <108039575+yuorei@users.noreply.github.com>
2024-04-18 11:08:19 +0900

    fix  a to CustomLink

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -1,10 +1,11 @@
+import CustomLink from "./custom-link";
 export default function Login() {
   return (
-    <a href="/login">
+    <CustomLink href="/login">
       <div className="flex items-center p-2 rounded-lg hover:bg-gray-700 group">
         <img src="/login.svg" alt="Login" className="w-6 h-6 rounded-full " />
         <span className="ml-2 text-white font-bold">ログイン</span>
       </div>
-    </a>
+    </CustomLink>
   );
 }
